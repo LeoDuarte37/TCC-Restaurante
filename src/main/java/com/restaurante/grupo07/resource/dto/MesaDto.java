e19f871744470ab57d132413a5e7044555b09917
Leo Duarte <leonardo.r.s.duarte@gmail.com>
2024-04-18 10:16:22 -0300

    Added new Dto's

---
+++ src/main/java/com/restaurante/grupo07/resource/dto/MesaDto.java
@@ -0,0 +1,13 @@
+package com.restaurante.grupo07.resource.dto;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Embeddable
+@Getter
+@Setter
+public class MesaDto {
+}
---
+++ src/main/java/com/restaurante/grupo07/resource/dto/PedidoDto.java
@@ -0,0 +1,14 @@
+package com.restaurante.grupo07.resource.dto;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Embeddable
+@Getter
+@Setter
+public class PedidoDto {
+
+}
