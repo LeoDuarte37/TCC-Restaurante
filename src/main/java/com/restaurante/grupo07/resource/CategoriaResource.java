f8407f05edb4a46f314361aac222b0d946962cb6
Leo Duarte <leonardo.r.s.duarte@gmail.com>
2024-04-25 19:00:22 -0300

    Desenvolvendo a camada Resource

---
+++ src/main/java/com/restaurante/grupo07/resource/CategoriaResource.java
@@ -0,0 +1,10 @@
+package com.restaurante.grupo07.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaResource {
+
+}
